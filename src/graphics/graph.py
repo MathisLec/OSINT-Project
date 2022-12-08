c91498cd5a34966c82a5f471d6b2f4c31b4ad1d1
MathisLec <mathis.lecoeuvre@gmail.com>
2022-12-08 18:28:43 +0100

    Refactor menu window + change style

--- src/graphics/graph.py
+++ src/graphics/graph.py
@@ -1,53 +1,58 @@
 import tkinter as tk
 
+BACKGROUND_COLOR = "#34495e"
+BUTTON_COLOR = "#bdc3c7"
+LABEL_COLOR = "#ecf0f1"
 
-class Application(tk.Frame):
-    def __init__(self, master=None):
-        super().__init__(master)
-        self.master = master
-        self.pack()
-        self.create_widgets()
+def buttonFactory(frame,comm,txt):
+    return tk.Button(frame,relief="flat",text=txt,bg=BUTTON_COLOR,font=("Arial Black",20),command=comm,height=2,width=10)
 
-    def create_widgets(self):
-        # DNSSCAN
-        self.dnsscan_button = tk.Button(self)
-        self.dnsscan_button["text"] = "DnsScan"
-        self.dnsscan_button["command"] = self.say_hi
-        self.dnsscan_button.pack(side="top")
-        # SHODAN
-        self.shodan_button = tk.Button(self)
-        self.shodan_button["text"] = "Shodan"
-        self.shodan_button["command"] = self.say_hi
-        self.shodan_button.pack(side="top")
-        # THEHARVESTER
-        self.theharvester_button = tk.Button(self)
-        self.theharvester_button["text"] = "TheHarvester"
-        self.theharvester_button["command"] = self.say_hi
-        self.theharvester_button.pack(side="top")
-        # URLSCAN
-        self.urlscan_button = tk.Button(self)
-        self.urlscan_button["text"] = "URLScan"
-        self.urlscan_button["command"] = self.say_hi
-        self.urlscan_button.pack(side="top")
-        # Domain Entry
-        self.domain = tk.Entry(self, textvariable="hello")
-        self.domain.focus_set()
-        self.domain.pack()
-        # Start Button
-        self.start_button = tk.Button(self)
-        self.start_button["text"] = "Start"
-        self.start_button["fg"] = "green"
-        self.start_button["command"] = self.say_hi
-        self.start_button.pack(side="bottom")
-        # Quit button
-        self.quit = tk.Button(self, text="QUIT", fg="red",
-                              command=self.master.destroy)
-        self.quit.pack(side="bottom")
+def startButtonFactory(frame, comm, txt):
+    button = buttonFactory(frame, comm, txt)
+    button["fg"] = "green"
+    button["activeforeground"] = "green"
+    return button
 
-    def say_hi(self):
-        print("hi there, everyone!")
+def say_hi():
+    print("hi there, everyone!")
+    
+# app frame
+app = tk.Tk()
+app.geometry("800x800")
+app.title("OSINT tool")
+app.configure(bg=BACKGROUND_COLOR)
 
-root = tk.Tk()
-root.geometry("300x200")
-app = Application(master=root)
+#label title
+menu_label = tk.Label(app,text="OSINT tool",font=("Arial Black",50),bg=BACKGROUND_COLOR,fg=LABEL_COLOR)
+menu_label.pack(expand=1)
+
+#menuButton frame
+menuButton_frame = tk.Frame(app,bg=BACKGROUND_COLOR)
+menuButton_frame.pack(expand=1)
+
+#DnsScan button
+dns_button = buttonFactory(menuButton_frame, say_hi, "DnsScan")
+dns_button.pack(side="left")
+
+#Shodan button
+shodan_button = buttonFactory(menuButton_frame, say_hi, "Shodan")
+shodan_button.pack(side="left")
+
+#TheHarvester button
+TH_button = buttonFactory(menuButton_frame, say_hi, "TheHarvester")
+TH_button.pack(side="left")
+
+#URLScan button
+URLScan_button = buttonFactory(menuButton_frame, say_hi, "URLScan")
+URLScan_button.pack(side="left")
+
+#Entry for the URL
+url_entry = tk.Entry(app,width=30,font=("Arial Black",20))
+url_entry.focus_set()
+url_entry.pack(expand=1)
+
+#Start button
+start_button = startButtonFactory(app, say_hi, "Start")
+start_button.pack(expand=1)
+
 app.mainloop()
