9ea756e82ae66de52e12396b1c75764a5a576707
MathisLec <mathis.lecoeuvre@gmail.com>
2022-12-11 19:36:50 +0100

    link to online service ok

--- src/main.py
+++ src/main.py
@@ -1,18 +1,9 @@
-import json
-import os
+import DnsScan
 
-def dnsScan():
-    with open(os.path.join(os.getcwd(),"resources","example.json"), 'r') as f:
-        j = json.load(f)
-        dns = j["dnsRecords"]
-        print("Nombre de services trouvés: "+str(len(dns)))
-        for l in dns:
-            print("Name: "+l["name"])
-            print("Type: "+l["dnsType"])
-            print("Raw text"+l["rawText"])
-            print("")
+def write(file_name, txt):
+    with open(os.path.join(os.getcwd(),"resources","example.json"), 'w+') as f:
+        f.write(txt)
 
 def main():
-    dnsScan()
-
+    DnsScan.scanL("www.google.fr")
 main()
